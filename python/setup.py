b4b7fd1661f97ade8d10428aa82c1986f61a690f
anakinxc <zhengyancheng.zyc@antgroup.com>
2024-03-20 16:48:55 +0800

    Bump version

--- python/setup.py
+++ python/setup.py
@@ -54,7 +54,7 @@ class BuildBazelExtension(build_ext.build_ext):
 
 setuptools.setup(
     name="sdc-sdk",
-    version="0.1.0b0",
+    version="0.2.0.dev20240320",
     author="secretflow",
     author_email="secretflow-contact@service.alipay.com",
     description="Secure Data Capsule SDK for python",
